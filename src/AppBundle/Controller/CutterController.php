71c2a1eb011157a96f653d4734fd98c9ee655797
Bariskas <Bariskas@gmail.com>
2016-06-21 04:51:58 +0300

    восстановлен функционал по обрезке видео
    восстановлен функционал по обрезке видео

--- src/AppBundle/Controller/CutterController.php
+++ src/AppBundle/Controller/CutterController.php
@@ -57,13 +57,39 @@
         }
 
         /**
+         * @Route("/cut-video", name="cutVideo")
+         */
+        public function cutAction(Request $request)
+        {
+            $id = $request->get('videoId');
+            $from = $request->request->get('from');
+            $to = $request->request->get('to');
+            $videoEntity = $this->getDoctrine()->getRepository('AppBundle:UserVideoEntity')
+                ->find($id);
+
+            $videoUtil = new VideoUtils($videoEntity, $this->container->getParameter('video_dir'));
+            $videoUtil->clip($from, $to);
+            $responseData = ['link' => '/upload/' . $videoEntity->getId() . '/' . 'clipped-' . $videoEntity->getName()];
+            return new Response(json_encode($responseData), 200, array('Content-Type'=>'application/json'));
+        }
+
+        /**
          * @Route("/upload/{id}/{name}", name="downloadVideo")
          */
         public function downloadAction($id, $name)
         {
             $file = Consts::VIDEO_DIR . $id . '/' . $name;
-            $response = new BinaryFileResponse($file);
+            $content = file_get_contents($file);
+
+            $response = new Response();
+
+            $response->headers->set('Content-Type', 'application/octet-stream');
+            $response->headers->set('Content-Disposition', 'attachment;filename="' . $name);
+
+            $response->setContent($content);
             return $response;
+            //$response = new BinaryFileResponse($file);
+            //return $response;
         }
 
     }
